e295a6c58328c6b262a3a593f3ed87118b042b47
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2022-09-20 18:06:29 -0300

    Mudanças no código pós correção. Ex.: Perda mínima

--- 05/perdaMin.js
+++ 05/perdaMin.js
@@ -1,12 +1,14 @@
 const precos = [30, 10, 54, 76, 1, 4, 35]
 
-let prejuizo = precos[0];
+let prejuizo = 9999999999999999999;
 
 for (let i = 0; i < precos.length; i++) {
     for (let j = (i + 1); j < precos.length; j++) {
         if (precos[j] < precos[i]) {
-            if ((precos[i] - precos[j]) < prejuizo) {
-                prejuizo = precos[i] - precos[j];
+            const precoCompra = precos[i];
+            const precoVenda = precos[j];
+            if ((precoCompra - precoVenda) < prejuizo) {
+                prejuizo = precoCompra - precoVenda;
             }
         }
     }
